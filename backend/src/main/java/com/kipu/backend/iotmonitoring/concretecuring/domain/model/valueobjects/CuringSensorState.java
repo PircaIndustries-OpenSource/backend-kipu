ca518dbe85b28ba2e4d1b00aacc4373b195e049b
Marce <marsredflag@gmail.com>
2026-06-15 15:11:53 -0500

    feat(iotmonitoring.concretecuring): add concrete curing sensor state enum.

---
+++ backend/src/main/java/com/kipu/backend/iotmonitoring/concretecuring/domain/model/valueobjects/CuringSensorState.java
@@ -0,0 +1,11 @@
+package com.kipu.backend.iotmonitoring.concretecuring.domain.model.valueobjects;
+
+/**
+ * Concrete Curing Sensor State Enum
+ * Represents the state of the IoT Concrete Curing Sensor
+ */
+public enum CuringSensorState {
+    INACTIVE,
+    MONITORING,
+    ALERT
+}
