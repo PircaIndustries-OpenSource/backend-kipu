0a3db2913b2d4380f3372e5c44d5366e5874a9f0
Marce <marsredflag@gmail.com>
2026-06-15 18:12:41 -0500

    feat(iotmonitoring.concretecuring): add get concrete curing sensor by id query.

---
+++ backend/src/main/java/com/kipu/backend/iotmonitoring/concretecuring/domain/model/queries/GetConcreteCuringSensorByIdQuery.java
@@ -0,0 +1,8 @@
+package com.kipu.backend.iotmonitoring.concretecuring.domain.model.queries;
+
+/**
+ * Get Concrete Curing Sensor By Identifier Query
+ * @param concreteCuringSensorId
+ */
+public record GetConcreteCuringSensorByIdQuery(Long concreteCuringSensorId) {
+}
