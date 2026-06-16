b96e7459fd68a5f148fe1480a42e670e2ad0dcd6
Marce <marsredflag@gmail.com>
2026-06-16 14:02:48 -0500

    feat(iotmonitoring.geolocalization): add get geolocalization sensor by id query

---
+++ backend/src/main/java/com/kipu/backend/iotmonitoring/geolocalization/domain/model/queries/GetGeolocalizationSensorByIdQuery.java
@@ -0,0 +1,7 @@
+package com.kipu.backend.iotmonitoring.geolocalization.domain.model.queries;
+
+/**
+ * Get Geolocalization Sensor By ID Query
+ */
+public record GetGeolocalizationSensorByIdQuery(Long geolocalizationSensorId) {
+}
