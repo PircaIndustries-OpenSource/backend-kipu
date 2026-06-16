dbb3afbe342263dc8b1df9359eef63c53d30e48b
Marce <marsredflag@gmail.com>
2026-06-16 13:52:25 -0500

    feat(iotmonitoring.geolocalization): add get all geolocalization sensors query.

---
+++ backend/src/main/java/com/kipu/backend/iotmonitoring/geolocalization/domain/model/queries/GetAllGeolocalizationSensorsQuery.java
@@ -0,0 +1,7 @@
+package com.kipu.backend.iotmonitoring.geolocalization.domain.model.queries;
+
+/**
+ * Get All Geolocalization Sensors Query
+ */
+public record GetAllGeolocalizationSensorsQuery() {
+}
