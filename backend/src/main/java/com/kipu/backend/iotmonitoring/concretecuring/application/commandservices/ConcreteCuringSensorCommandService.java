6e7124522a26d570e3e8796a630bc02ac3480ad8
Marce <marsredflag@gmail.com>
2026-06-15 22:08:43 -0500

    feat(iotmonitoring.concretecuring): add concrete curing sensor command service to handle commands.

---
+++ backend/src/main/java/com/kipu/backend/iotmonitoring/concretecuring/application/commandservices/ConcreteCuringSensorCommandService.java
@@ -0,0 +1,21 @@
+package com.kipu.backend.iotmonitoring.concretecuring.application.commandservices;
+
+import com.kipu.backend.iotmonitoring.concretecuring.domain.model.aggregates.ConcreteCuringSensor;
+import com.kipu.backend.iotmonitoring.concretecuring.domain.model.commands.CreateConcreteCuringSensorCommand;
+import com.kipu.backend.shared.application.result.ApplicationError;
+import com.kipu.backend.shared.application.result.Result;
+
+/**
+ * Concrete Curing Command Service
+ */
+public interface ConcreteCuringSensorCommandService {
+
+    /**
+     * Handle Create Concrete Curing Command
+     *
+     * @param command The {@link CreateConcreteCuringSensorCommand} Command
+     * @return A {@link Result} containing the created {@link ConcreteCuringSensor} on success,
+     * or an {@link ApplicationError} on failure (validation or business rule violation)
+     */
+    Result<ConcreteCuringSensor, ApplicationError> handle(CreateConcreteCuringSensorCommand command);
+}
