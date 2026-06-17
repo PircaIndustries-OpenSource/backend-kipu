815a9a3df4a86867954199208b332efc9b97dfa7
Marce <marsredflag@gmail.com>
2026-06-16 20:55:57 -0500

    feat(iotmonitoring.hopperwatch): add get hopper watch sensor by id query.

---
+++ backend/src/main/java/com/kipu/backend/iotmonitoring/hopperwatch/domain/model/queries/GetHopperWatchSensorByIdQuery.java
@@ -0,0 +1,7 @@
+package com.kipu.backend.iotmonitoring.hopperwatch.domain.model.queries;
+
+/**
+ * Get Hopper Watch By ID Query
+ */
+public record GetHopperWatchSensorByIdQuery(Long hopperWatchId) {
+}
